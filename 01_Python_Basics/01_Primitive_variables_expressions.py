abc3d0c3e8d736cd1750406940d495b50711914f
praveenuk7 <praveenuk9@gmail.com>
2023-09-22 22:00:13 +0530

    day1

---
+++ 01_Python_Basics/01_Primitive_variables_expressions.py
@@ -0,0 +1,17 @@
+"""
+Python provides a collection of primitive types such as
+int -- 42
+float -- 4.2
+str -- "forty two"
+bool -- True
+"""
+
+'''
+A variable is a name that refers to a value and value represents an object of some type
+'''
+x = 42
+print(x)
+
+
+y : int = 32
+print(y)
